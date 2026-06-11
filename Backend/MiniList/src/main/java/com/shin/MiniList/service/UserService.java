45b1dec75c871f6858bb5da29071101a34e2ac14
Shin <sadiqmohammad201@gmail.com>
2026-06-11 13:42:43 +0530

    add Bcrypt password Hashing for registration endpoint

--- Backend/MiniList/src/main/java/com/shin/MiniList/service/UserService.java
+++ Backend/MiniList/src/main/java/com/shin/MiniList/service/UserService.java
@@ -3,6 +3,7 @@ package com.shin.MiniList.service;
 import com.shin.MiniList.model.Users;
 import com.shin.MiniList.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,12 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users users) {
+
+        users.setPassword(encoder.encode(users.getPassword()));
+
         return repo.save(users);
     }
 
